d2d2546f9ba525f4a8226bd71a6e2e61634e64b1
taiane94sb <taiane19sb94@gmail.com>
2023-10-26 15:33:21 -0300

    first commit

---
+++ src/main/java/br/com/taianesb/javastreamapi/JavaStreamApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.taianesb.javastreamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStreamApiApplication.class, args);
+	}
+
+}
